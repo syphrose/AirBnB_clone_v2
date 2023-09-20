cf5c4a9a50a0a63f804118e0a6d500cfccff5fbd
syphrose <swabulwenyi@gmail.com>
2023-09-20 04:17:02 -0700

    Updating user, place & city

--- models/city.py
+++ models/city.py
@@ -3,6 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Integer, Column, String
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship 
+from models.place import Place
+from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(BaseModel, Base):
@@ -10,3 +13,4 @@ class City(BaseModel, Base):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan', backref="cities")
