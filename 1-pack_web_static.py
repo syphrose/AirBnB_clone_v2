8117929abd5cae37006d91dbedb975c2b4e2159c
syphrose <swabulwenyi@gmail.com>
2023-10-08 15:48:12 -0700

    Compressing the contents

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from time import strftime
+from datetime import date
+from fabric.api import local
+
+
+def do_pack():
+    """ A script that generates a .tgz archive from the contents of web_static folder using do_pack func"""
+
+    filename = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filename))
+
+        return "versions/web_static_{}.tgz".format(filename)
+
+    except Exception as e:
+        return None
